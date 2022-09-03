7eb776a0c9104ba57e2db3d3da985fc363c93b55
Sourav Saha <shanto.sourav07@gmail.com>
2022-09-03 20:02:19 +0600

    Display News Done

--- js/main.js
+++ js/main.js
@@ -48,17 +48,24 @@ const newsDisplay = (allNews) => {
         `;
     }
 
-    for (const news of allNews) {
+    //sorting news by view
+    let sort = allNews.sort((a, b) => {
+        const aView = a?.total_view;
+        const bView = b?.total_view;
+        return (aView < bView) ? 1 : -1;
+    });
+
+    for (const news of sort) {
         notFound.innerHTML = "";
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
             <div class="card mb-3">
-                <div class="row g-0">
-                    <div class="col-sm-12 text-center col-md-5">
-                        <img src="${news.thumbnail_url}" class="w-100 h-100" alt="...">
+                <div class="row g-3">
+                    <div class="col-12 text-center col-md-5">
+                        <img  style="width: 300px; height: 250px;" src="${news.thumbnail_url}" alt="...">
                     </div>
-                    <div class="col-sm-12 col-md-7">
+                    <div class="col-12 col-md-7">
                         <div class="card-body">
                             <h5 class="card-title">${news.title}</h5>
                             <p class="card-text">${news.details.slice(0, 120)}....</p>
@@ -87,6 +94,43 @@ const newsDisplay = (allNews) => {
     }
     const spinnerSection = document.getElementById("spinner");
     spinnerSection.classList.add("d-none");
+};
+
+const showCategoryDetails = (news_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => categoryDetails(data.data[0]))
+        .catch((error) => console.log(error));
+};
+
+// Load category details with modal
+const categoryDetails = (details) => {
+    const title = document.getElementById("exampleModalLabel");
+    title.innerText = details.title;
+    const modal = document.getElementById("modal-img");
+    modal.innerHTML = `
+      <img src="${details.image_url}" alt="">
+    `;
+    const modalDetails = document.getElementById("modal-details");
+    modalDetails.innerText = details.details.slice(0, 200) + "...";
+    const modalAuthor = document.getElementById("modal-author");
+    if (details.author.name === null) {
+        modalAuthor.innerText = "No author";
+    } else {
+        modalAuthor.innerText = "Author:" + " " + details.author.name;
+    }
+    const modalViews = document.getElementById("modal-views");
+    if (details.total_view === null) {
+        modalViews.innerText = "No views";
+    } else {
+        modalViews.innerText = "Total View:" + " " + details.total_view;
+    }
+    if (details.author.published_date === null) {
+        document.getElementById("date").innerText = "No date found";
+    } else {
+        document.getElementById("date").innerText = details.author.published_date;
+    }
 };
 
 allCategories();
